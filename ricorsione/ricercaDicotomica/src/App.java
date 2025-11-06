e8d86204a36ee2b2fcf0a997b041ef7bbb3ee600
Mattia Todesco <mattia.todesco@marconirovereto.it>
2025-11-06 11:58:37 +0100

    Aggiungi ordinamento ricorsivo, da sistemare

--- ricorsione/ricercaDicotomica/src/App.java
+++ ricorsione/ricercaDicotomica/src/App.java
@@ -16,4 +16,76 @@ public class App {
         }
 
     }
+
+    public static int cercaMinimo(int[] v, int minimo, int ultimo){
+
+        //clausola di chiusura, se ho controllato tutto l'array
+        if (ultimo == 0)
+            return minimo;
+        
+        if (minimo > v[ultimo])
+            minimo = v[ultimo];
+
+        return cercaMinimo(v, minimo, ultimo);
+    }
+
+    public static int cercaUltimoBetter(int[] v, int ultimo){
+        //clausola ricorsiva
+        if (ultimo == 0)
+            return v[ultimo];
+        //chiamata ricorsiva
+        return Math.min(v[ultimo], cercaUltimoBetter(v, ultimo-1));
+    }
+
+    public static void bubbleSort(int[] v, int fine){
+        //clausola ricorsiva
+            if (fine == 0) //se l'array è lungo 1, è gia ordinato di default
+                return;
+        //analisi dell'array e spostamento del max in fondo
+            for (int i = 0; i < fine-1; i++){
+                if (v[i] > v[i+1])
+                    swap(v, i, i+1);
+            }
+        //chiamata ricorsiva
+        bubbleSort(v, fine -1);
+    }
+
+    public static void shakerSort(int[] v, int inizio){
+
+    }
+
+    //Da sistemare magari ecco
+    public static void shakerSortLeft(int[] v, int inizio, int fine){
+        //clausola ricorsiva
+        if (inizio >= fine) //se l'array è lungo 1, è gia ordinato di default
+            return;
+        //analisi dell'array e spostamento del max in fondo
+            for (int i = 0; i < fine-1; i++){
+                if (v[i] > v[i-1])
+                    swap(v, i, i+1);
+            }
+        //chiamata ricorsiva
+        shakerSortRight(v, inizio +1, fine);        
+    }
+
+    public static void shakerSortRight(int[] v, int inizio, int fine){
+        //clausola ricorsiva
+        if (inizio >= fine)
+            return; //se l'array è lungo 1, è gia ordinato di default
+        //analisi dell'array e spostamento del max in fondo
+            for (int i = inizio; i < fine; i++){
+                if (v[i] > v[i+1]){
+                    swap(v, i, i+1);
+                }
+            }
+        //chiamata ricorsiva
+        shakerSortLeft(v, inizio, fine -1);
+    }
+
+    public static void swap(int[] v, int i, int j){
+        int temp;
+        temp = v[i];
+        v[i] = v[j];
+        v[j] = temp;
+    }
 }
